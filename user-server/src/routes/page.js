cff4a7f2e28d64e520bf1e34118f8975c882e9d3
Kemono BAT4 <kemono.bat.4@gmail.com>
2023-02-16 22:57:30 +0100

    inizio test per invio dati all' altro server

--- user-server/src/routes/page.js
+++ user-server/src/routes/page.js
@@ -2,36 +2,29 @@ const express = require("express")
 const pageRouter = express.Router()
 
 pageRouter.get("/", async(req,res) => {
-
-    //console.log(req.query)
-    //let query = req.query.q
-    //console.log(query)
-
+    
     res.render("index")
 })
 
 pageRouter.get("/settings", async(req,res) => {
-
-    //console.log(req.query)
-    //let query = req.query.q
-    //console.log(query)
-
     res.render("settings")
 })
 
 pageRouter.get("/login", async(req,res) => {
-
-    //console.log(req.query)
-    //let query = req.query.q
-    //console.log(query)
 
     res.render("login")
 })
 
 pageRouter.post("/test", function (req, res){
     
-    
-    //console.log(req.body)
+    fetch("localhost:5000/users/banana").then(res =>{
+
+        return res.json();
+    }).then(response=>{
+
+        console.log(response)
+    })
+
     res.send(JSON.stringify(req.body));
 })
 
