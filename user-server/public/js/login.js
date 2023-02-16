48141d793a0058807ca897d0cd2d098c970463ae
Kemono BAT4 <kemono.bat.4@gmail.com>
2023-02-16 22:56:13 +0100

    modifica metodo di invio informazioni al server, da finire

--- user-server/public/js/login.js
+++ user-server/public/js/login.js
@@ -1,24 +1,40 @@
-/*
+let form = document.querySelector(".login-form")
+let submit = document.querySelector(".submit-button")
 
-function login(){
-    fetch('https://reqbin.com/echo/post/json', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ "id": 78912 })
+localStorage.setItem("username", "test")
+console.log(localStorage.getItem("username"))
+
+submit.addEventListener("click", event =>{
+    event.preventDefault()
+
+    let username = "this is a username"
+    let password = "this is a password"
+
+    fetch("/test", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "username": + ""+username, "password": + ""+password})
+    })
+    .then(response => {
+        return response.json()
+    }).then(response => {
+        if(response != null){
+            if(response.username === "true"){
+                document.location = "/"
+            }else{
+                accessDenied("Wrong username or password", 1)
+            }
+        }else{
+            accessDenied("can't connect to the server, check your connection or the server status", 2)
+        }
+        console.log(response)
+    })
 })
-.then(response => response.json())
-.then(response => console.log(JSON.stringify(response)))
-}
-*/
 
-/*var xhttp = new XMLHttpRequest();
-xhttp.open("POST", "/test", false);
-const body = new URLSearchParams();
-body.append("test", "test");
-xhttp.send(body);
-window.location.reload();
-*/
-//let username = localStorage.setItem("username", )
+function accessDenied(reason, id){
+
+
+}
