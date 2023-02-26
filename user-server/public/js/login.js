c7ba19af60441927d1ca94219c3bd0a1b3b6bf5c
Kemono BAT4 <kemono.bat.4@gmail.com>
2023-02-26 18:16:06 +0100

    modifiche a js e ejs del login, fixati errori di login

--- user-server/public/js/login.js
+++ user-server/public/js/login.js
@@ -1,27 +1,20 @@
-let form = document.querySelector(".login-form")
+
 let submit = document.querySelector(".submit-button")
 
 localStorage.setItem("username", "test")
-console.log(localStorage.getItem("username"))
+//console.log(localStorage.getItem("username"))
 
 submit.addEventListener("click", event =>{
     event.preventDefault()
 
-    let username = "this is a username"
-    let password = "this is a password"
+    let username = document.querySelector(".user-text").value
+    let password = document.querySelector(".password-text").value
 
-    fetch("/test", {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "username": + ""+username, "password": + ""+password})
-    })
-    .then(response => {
-        return response.json()
-    }).then(response => {
-        if(response != null){
+    //let data = {'name': 'John Doe', 'occupation': 'John Doe'}
+    let data = {'username': " " + username + " " , 'password' : " "+ password + " "}
+    doRequest(data)
+
+        /*if(response != null){
             if(response.username === "true"){
                 document.location = "/"
             }else{
@@ -29,12 +22,33 @@ submit.addEventListener("click", event =>{
             }
         }else{
             accessDenied("can't connect to the server, check your connection or the server status", 2)
-        }
-        console.log(response)
-    })
+        }*/
+        //console.log(response)
+    //})
 })
+
+async function doRequest(data){
+    
+    
+    let response = await fetch("/test", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response =>{
+
+        return response.json();
+    }).then(res =>{
+        
+        console.log(res)
+    })
+}
+
 
 function accessDenied(reason, id){
 
+    
 
 }
